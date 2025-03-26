e8054b62abd2ddb3b64a15eb626def1cf455dd88
Ojas Kandhare <ojaskandhare@gmail.com>
2025-03-25 23:43:49 -0700

    Fix WebSocket handling for production deployment

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from src.server import app, socketio
 
+# This is needed for gunicorn
+application = socketio.middleware(app)
+
 if __name__ == "__main__":
     socketio.run(app, debug=True, allow_unsafe_werkzeug=True) 
